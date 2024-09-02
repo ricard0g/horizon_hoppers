963f5a405dd9be5f15270ed25864c954a2aeb92b
Ricardo Guzman <ricardoguzman@Ricardos-MacBook-Air.local>
2024-09-02 18:39:28 +0200

    Search Logic shows correct city in the console log

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,17 +2,33 @@
 const searchResults = [];
 
 async function fetchResults() {
-    try {
-        const inputValue = document.getElementById("search").value.toLowerCase();
-        
-        if (inputValue) {
-            const response = await fetch('./travel_recommendation_api.json');
+	try {
+		const inputValue = document.getElementById("search").value.toLowerCase();
 
-            if (response) {
-                const jsonResponse = await response.json();
-            }
-        }
-    } catch(error) {
-        console.log(error)
-    }
+		if (inputValue) {
+			const response = await fetch("./travel_recommendation_api.json");
+
+			if (response) {
+				const jsonResponse = await response.json();
+
+				showFilteredResults(jsonResponse, inputValue);
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+// Display Results
+
+function showFilteredResults(travelRecommendationJson, inputValue) {
+	const citiesArr = []
+
+    travelRecommendationJson.countries.map(country => {
+        country.cities.forEach(city => citiesArr.push(city));
+    })
+
+    const cityFound = citiesArr.find(city => city.name.toLowerCase().includes(inputValue) === true);
+
+    console.log(cityFound);
 }
